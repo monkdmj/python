9513e690dd76b52d0e0672f8233364adcd119c41
monkdmj <monkdmj@gmail.com>
2016-11-16 15:48:57 +0800

    test3.py pep8 modification

--- test3.py
+++ test3.py
@@ -22,14 +22,20 @@ def fn(x, y):
 
 
 def char2num(s):
-    return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+    return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
+            '8': 8, '9': 9}[s]
 
 
 def str2int(s):
     from functools import reduce
     # return reduce(fn,lirst(map(char2num,s)))
     # return reduce(lambda x, y: x * 10 + y, map(char2num, s))
-    return reduce(lambda x, y: x * 10 + y, map(lambda s: {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s], s))
+    return reduce(lambda x, y: x * 10 + y, map(lambda s: {'0': 0, '1': 1,
+                                                          '2': 2, '3': 3,
+                                                          '4': 4, '5': 5,
+                                                          '6': 6, '7': 7,
+                                                          '8': 8, '9': 9
+                                                          }[s], s))
 
 
 def normalize(name):
@@ -40,6 +46,7 @@ def str2float(s):
     from functools import reduce
     intpart, decpart = s.split('.')
     decpart = decpart[::-1]
-    return reduce(lambda x, y: x * 10 + y, map(char2num, intpart)) + reduce(lambda x, y: x / 10 + y, map(char2num, decpart)) / 10
+    return reduce(lambda x, y: x * 10 + y, map(char2num, intpart)) \
+        + reduce(lambda x, y: x / 10 + y, map(char2num, decpart)) / 10
 
 # help(str.split)
